239ca5bf9c325f732095c828c536bf8eedfbe700
kdcross <kenneth.d.cross@gmail.com>
2022-10-24 02:18:48 -0500

    moved card into restaurantinfo to use props

--- ReactNative/MealsToGo/src/features/restaurants/components/cards.js
+++ ReactNative/MealsToGo/src/features/restaurants/components/cards.js
@@ -1,11 +1,13 @@
 import * as React from 'react';
+import { Text } from 'react-native';
 import { Avatar, Button, Card, Title, Paragraph} from 'react-native-paper';
+import { RestaurantInfo } from './restaurant-info.component';
 
 export const RestaurantCard = () => (
     <Card>
         <Card.Content>
             <Card.Cover source={{ uri: 'https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg' }} />
-            <Title>Some Restaurant</Title>
+            <Text>{name}</Text>
         </Card.Content>
     </Card>
 );
--- ReactNative/MealsToGo/src/features/restaurants/components/restaurant-info.component.js
+++ ReactNative/MealsToGo/src/features/restaurants/components/restaurant-info.component.js
@@ -1,16 +1,34 @@
 import React from "react";
-import { Text } from "react-native";
+import { StyleSheet, Text } from "react-native";
+import { Card } from "react-native-paper";
 import { RestaurantCard } from "./cards.js";
 
 export const RestaurantInfo = ({ restaurant = {} }) => {
-    //const {
-        //name = 'Some Restaurant',
-        //icon,
-        //photos = ["https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg"],
-        //address = "100 some random street",
-        //isOpenNow = true,
-        //rating = 4,
-        //isClosedTemporarily
-    //} = restaurant;
-    return (<RestaurantCard />)
-}
+  const {
+    name = "Some Restaurant",
+    icon,
+    photos = [
+      "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
+    ],
+    address = "100 some random street",
+    isOpenNow = true,
+    rating = 4,
+    isClosedTemporarily,
+  } = restaurant;
+  
+  return (
+    <Card elevation={8} style={styles.card}>
+      <Card.Content>
+        <Card.Cover key={name} style={styles.cover} source={{ uri: photos[0] }} />
+        <Text>{name}</Text>
+      </Card.Content>
+    </Card>
+  );
+
+  
+};
+
+const styles = StyleSheet.create({
+    card: { backgroundColor: "white"},
+    cover: {padding: 20, backgroundColor: "white"},
+});
