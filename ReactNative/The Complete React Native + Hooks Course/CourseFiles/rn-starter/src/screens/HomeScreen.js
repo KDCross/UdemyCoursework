8586137a22d1ee01a950a8d2e38514befbed5496
kdcross <kenneth.d.cross@gmail.com>
2023-03-13 02:35:47 -0500

    .

--- ReactNative/The Complete React Native + Hooks Course/CourseFiles/rn-starter/src/screens/HomeScreen.js
+++ ReactNative/The Complete React Native + Hooks Course/CourseFiles/rn-starter/src/screens/HomeScreen.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Text, StyleSheet } from "react-native";
 
 const HomeScreen = () => {
+  //The exercise was to change the text from "HomeScreen." The course used "Hi There." I went a different direction.
   return <Text style={styles.text}>I Like Potatoes</Text>;
 };
 
