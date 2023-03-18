bec238248d54732e402c7f6dee28a73cd053e6cb
kdcross <kenneth.d.cross@gmail.com>
2023-03-18 03:11:45 -0500

    Completed Section 4

--- ReactNative/The Complete React Native + Hooks Course/CourseFiles/rn-starter/src/screens/HomeScreen.js
+++ ReactNative/The Complete React Native + Hooks Course/CourseFiles/rn-starter/src/screens/HomeScreen.js
@@ -1,14 +1,30 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
-const HomeScreen = () => {
+const HomeScreen = ({navigation}) => {
   //The exercise was to change the text from "HomeScreen." The course used "Hi There." I went a different direction.
-  return <Text style={styles.text}>I Like Potatoes</Text>;
+  return (
+    <View>
+      <Text style={styles.text}>I like Potatoes!</Text>
+      <Button
+        title="Go to Components Demo"
+        onPress={() => navigation.navigate("Components")}
+      />
+      <Button
+        title="Go to List Demo"
+        onPress={() => navigation.navigate("List")}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   text: {
     fontSize: 30,
+  },
+  button: {
+    fontSize: 20,
+    color: "blue",
   },
 });
 
