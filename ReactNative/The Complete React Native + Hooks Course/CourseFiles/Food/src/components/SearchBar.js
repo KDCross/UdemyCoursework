a3381c9ef866a6275f21e6e4d39ad375aee4f3bb
kdcross <kenneth.d.cross@gmail.com>
2023-05-15 18:18:30 -0500

    Completed Section 8

---
+++ ReactNative/The Complete React Native + Hooks Course/CourseFiles/Food/src/components/SearchBar.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { StyleSheet, TextInput, View } from "react-native";
+import { Feather } from "@expo/vector-icons";
+
+export const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
+  return (
+    <View style={styles.backgroundStyle}>
+      <Feather style={styles.iconStyle} name="search" />
+      <TextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        placeholder="Search"
+        style={styles.inputStyle}
+        value={term}
+        onChangeText={onTermChange}
+        onEndEditing={onTermSubmit}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  backgroundStyle: {
+    flexDirection: "row",
+    backgroundColor: "#F0EEEE",
+    height: 50,
+    borderRadius: 5,
+    marginTop: 10,
+  },
+  iconStyle: {
+    fontSize: 35,
+    alignSelf: "center",
+    marginHorizontal: 5,
+  },
+  inputStyle: {
+    borderColor: "black",
+    borderWidth: 1,
+    flex: 1,
+    fontSize: 18,
+  },
+});
